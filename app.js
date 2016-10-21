6711d572ece6f56caa7a1d868d95ed93b88b7da6
zza00000 <superbear24@163.com>
2016-10-21 11:07:33 +0800

    完成日志输出配置

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import bunyan from 'bunyan';
+import fs from 'fs-extra';
+import moment from 'moment';
 
 /***** import own libraries *****/
 import config from './app/configs/index';
@@ -14,6 +17,26 @@ import router from './app/routes/routes';
 
 const app = express();
 const RedisStore = connectRedis(session);
+
+/*********判断日志文件是否存在，每天新建一个日志文件，名字为当天时间*********/
+let data = new moment(Date.now()).format('YYYY-MM-DD');
+let file = 'logger/' + data.toString() + '.log';
+fs.ensureFile(file, function(err){
+	if(err) console.log(err);
+});
+console.log(file);
+
+/*********日志配置*********/
+const log = bunyan.createLogger({
+						name: 'hostlocal',
+						streams: [{
+							path: file
+						}]
+					});
+//TODO 打印的日志时间没有格式化，日志文件大小还不知道如何确定
+
+//打印第一天日志
+log.info('login : ' + data.toString());
 
 /*********express设置*********/
 app.set('views', path.join(__dirname, 'app/views/pages'))
@@ -35,7 +58,6 @@ app
 
 /*********路由*********/
 router(app);
-
 
 /*********服务器监听端口*********/
 app.listen(config.portConfig, () =>{
